81f60b11e7a660dbba157fcc1b654d26ec736976
Garkatron <masitacarg@gmail.com>
2026-06-19 16:52:51 +0100

    fixed?

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", {}]],
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
+
   base: "/EsUn10pero-Frontend/",
+
   server: {
     port: 5173,
     proxy: {
